922d279ed4d2fc4bf0065faa69bc2db2e8f51315
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-10 23:19:58 +0700

    Поменял формат хранения типа вопроса число->строка

--- src/main/java/ru/kosterror/forms/entity/question/QuestionEntity.java
+++ src/main/java/ru/kosterror/forms/entity/question/QuestionEntity.java
@@ -1,8 +1,6 @@
 package ru.kosterror.forms.entity.question;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import ru.kosterror.forms.entity.BaseEntity;
@@ -17,6 +15,7 @@ public abstract class QuestionEntity extends BaseEntity {
 
     private String comment;
 
+    @Enumerated(value = EnumType.STRING)
     private QuestionType type;
 
     private int points;
