afc345588fbb8e90908501f472eb32dbb780b4cc
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-12 14:48:33 +0700

    Инициализировал file-storage-service

---
+++ file-storage-service/src/main/java/ru/kosterror/forms/filestorageservice/FileStorageService.java
@@ -0,0 +1,13 @@
+package ru.kosterror.forms.filestorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileStorageService.class, args);
+    }
+
+}
