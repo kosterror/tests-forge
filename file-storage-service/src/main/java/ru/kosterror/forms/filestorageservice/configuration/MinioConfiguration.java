e21ed31799a30c004a18a712b6621417c2bb25fa
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-12 18:46:52 +0700

    Доработал конфигурацию MinioConfiguration
    Теперь не нужно вручную создавать бакет в MinIO. Если во время запуска сервиса бакет не будет найден, то сервис сам его создаст

--- file-storage-service/src/main/java/ru/kosterror/forms/filestorageservice/configuration/MinioConfiguration.java
+++ file-storage-service/src/main/java/ru/kosterror/forms/filestorageservice/configuration/MinioConfiguration.java
@@ -1,10 +1,15 @@
 package ru.kosterror.forms.filestorageservice.configuration;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class MinioConfiguration {
@@ -13,10 +18,32 @@ public class MinioConfiguration {
 
     @Bean
     public MinioClient minioClient() {
-        return MinioClient.builder()
+        var minioClient = MinioClient.builder()
                 .endpoint(minioProperties.url())
-                .credentials(minioProperties.secretKey(), minioProperties.secretKey())
+                .credentials(minioProperties.accessKey(), minioProperties.secretKey())
                 .build();
+
+        checkBucketExistence(minioClient);
+
+        return minioClient;
+    }
+
+    @SneakyThrows
+    private void checkBucketExistence(MinioClient minioClient) {
+        var isBucketExists = minioClient.bucketExists(BucketExistsArgs.builder()
+                .bucket(minioProperties.bucket())
+                .build());
+
+        if (!isBucketExists) {
+            log.info("Bucket {} does not exist. Creating...", minioProperties.bucket());
+            minioClient.makeBucket(MakeBucketArgs.builder()
+                    .bucket(minioProperties.bucket())
+                    .build()
+            );
+            log.info("Bucket {} created", minioProperties.bucket());
+        } else {
+            log.info("Bucket {} already exists", minioProperties.bucket());
+        }
     }
 
 }
