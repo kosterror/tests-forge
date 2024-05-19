4e3eb94669b53b9ff40af1de3f16198261705aa9
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-19 21:56:19 +0700

    Добавил конфигурацию для swagger-ui в core-service

---
+++ core-service/src/main/java/ru/kosterror/forms/coreservice/config/SpringDocConfiguration.java
@@ -0,0 +1,36 @@
+package ru.kosterror.forms.coreservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfiguration {
+
+    public static final String JWT = "Bearer Token";
+    private static final String JWT_BEARER_FORMAT = "JWT";
+    private static final String JWT_SCHEME = "bearer";
+    private static final String TITLE = "core-service API";
+    private static final String VERSION = "1.0.0";
+
+    @Bean
+    public OpenAPI userServiceOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title(TITLE)
+                .version(VERSION)
+
+        ).components(new Components()
+                .addSecuritySchemes(JWT,
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme(JWT_SCHEME)
+                                .bearerFormat(JWT_BEARER_FORMAT)
+                )
+        );
+    }
+
+
+}
