e204f5fc92d658e7ae0c9c9a9ef96abe0be63989
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-23 01:51:34 +0700

    Убрал логирование дефолтного пароля для Spring Security

--- core-service/src/main/java/ru/kosterror/forms/coreservice/CoreServiceApplication.java
+++ core-service/src/main/java/ru/kosterror/forms/coreservice/CoreServiceApplication.java
@@ -2,8 +2,9 @@ package ru.kosterror.forms.coreservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class CoreServiceApplication {
 
     public static void main(String[] args) {
