cc5a856c764d4b656c0a532ce6acc54913286b65
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-23 01:18:32 +0700

    Добавил аутентификацию по api-key

---
+++ security-starter/src/main/java/ru/kosterror/forms/securitystarter/configuration/ApiKeyAutoConfiguration.java
@@ -0,0 +1,20 @@
+package ru.kosterror.forms.securitystarter.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import ru.kosterror.forms.securitystarter.security.ApiKeyAuthenticationConverter;
+
+@AutoConfiguration
+public class ApiKeyAutoConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(name = "security.api-key.autoconfigure", havingValue = "true")
+    public ApiKeyAuthenticationConverter apiKeyAuthenticationConverter(
+            @Value("${security.api-key.key}") String apiKey
+    ) {
+        return new ApiKeyAuthenticationConverter(apiKey);
+    }
+
+}
