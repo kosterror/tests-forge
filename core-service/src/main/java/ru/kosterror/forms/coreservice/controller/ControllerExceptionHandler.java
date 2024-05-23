234f3ee8971d88442b899e1e5b425b07918842fb
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-24 00:47:06 +0700

    Добавил новое исключение и поправил их обработку в core-service

--- core-service/src/main/java/ru/kosterror/forms/coreservice/controller/ControllerExceptionHandler.java
+++ core-service/src/main/java/ru/kosterror/forms/coreservice/controller/ControllerExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.kosterror.forms.commonmodel.ErrorResponse;
+import ru.kosterror.forms.coreservice.exception.BadRequestException;
 import ru.kosterror.forms.coreservice.exception.NotFoundException;
 
 import java.util.ArrayList;
@@ -54,17 +55,31 @@ public class ControllerExceptionHandler {
                     }
                 });
 
-        ErrorResponse validationError = new ErrorResponse(
+        ErrorResponse response = new ErrorResponse(
                 HttpStatus.BAD_REQUEST,
                 "Validation error",
                 request.getRequestURI(),
                 errors
         );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(validationError);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(HttpServletRequest request,
+                                                                   BadRequestException exception) {
+        logException(request, exception);
+
+        ErrorResponse response = buildErrorResponse(exception.getMessage(),
+                request.getRequestURI(),
+                HttpStatus.BAD_REQUEST
+        );
+
+        return ResponseEntity.status(response.getCode()).body(response);
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(HttpServletRequest request, NotFoundException exception) {
+    public ResponseEntity<ErrorResponse> handleNotFoundException(HttpServletRequest request,
+                                                                 NotFoundException exception) {
         logException(request, exception);
 
         ErrorResponse response = buildErrorResponse(exception.getMessage(),
@@ -72,11 +87,12 @@ public class ControllerExceptionHandler {
                 HttpStatus.NOT_FOUND
         );
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        return ResponseEntity.status(response.getCode()).body(response);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(HttpServletRequest request, Exception exception) {
+    public ResponseEntity<ErrorResponse> handleException(HttpServletRequest request,
+                                                         Exception exception) {
         logException(request, exception);
 
         ErrorResponse response = buildErrorResponse("Internal service error",
@@ -84,7 +100,7 @@ public class ControllerExceptionHandler {
                 HttpStatus.INTERNAL_SERVER_ERROR
         );
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        return ResponseEntity.status(response.getCode()).body(response);
     }
 
 }
