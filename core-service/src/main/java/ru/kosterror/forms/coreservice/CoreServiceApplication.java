04163bc8beccc1a875fa09df3dde64aa22957247
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-06-02 20:50:09 +0700

    Включил генерацию значений для полей под @LastModifiedDate и @CreatedDate в core-service

--- core-service/src/main/java/ru/kosterror/forms/coreservice/CoreServiceApplication.java
+++ core-service/src/main/java/ru/kosterror/forms/coreservice/CoreServiceApplication.java
@@ -3,8 +3,10 @@ package ru.kosterror.forms.coreservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableJpaAuditing
 public class CoreServiceApplication {
 
     public static void main(String[] args) {
