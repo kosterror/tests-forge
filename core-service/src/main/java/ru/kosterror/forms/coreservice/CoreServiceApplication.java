8db2b680d5081fc9c84da1f02b32b2fe1eb74453
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-06-11 02:10:26 +0700

    Добавил SecurityAuditorAware

--- core-service/src/main/java/ru/kosterror/forms/coreservice/CoreServiceApplication.java
+++ core-service/src/main/java/ru/kosterror/forms/coreservice/CoreServiceApplication.java
@@ -3,10 +3,8 @@ package ru.kosterror.forms.coreservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
-@EnableJpaAuditing
 public class CoreServiceApplication {
 
     public static void main(String[] args) {
