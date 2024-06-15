3e4ed2af25af7a0c36c7ad87af0c051252ef032e
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-06-16 02:57:49 +0700

    Добавил создание топика через mail-service

--- mail-service/src/main/java/ru/kosterror/forms/mailservice/MailServiceApplication.java
+++ mail-service/src/main/java/ru/kosterror/forms/mailservice/MailServiceApplication.java
@@ -2,8 +2,10 @@ package ru.kosterror.forms.mailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MailServiceApplication {
 
     public static void main(String[] args) {
