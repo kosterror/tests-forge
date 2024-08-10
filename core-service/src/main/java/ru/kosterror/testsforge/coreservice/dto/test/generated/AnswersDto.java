2dc9912e58812745f11070a3464e9cbf87904816
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-08-10 15:08:35 +0700

    Добавил DTO с ответами на вопросы

---
+++ core-service/src/main/java/ru/kosterror/testsforge/coreservice/dto/test/generated/AnswersDto.java
@@ -0,0 +1,41 @@
+package ru.kosterror.testsforge.coreservice.dto.test.generated;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+public record AnswersDto(
+
+        @NotNull(message = "Не может быть null, должно быть хотя бы пустым")
+        @Schema(description = "Мапа для ответов на вопросы с единственным выбором. " +
+                "Должна быть хотя бы пустой, но не null. Ключ - ID вопроса, значение - ID ответа",
+                requiredMode = REQUIRED)
+        Map<UUID, @NotNull UUID> singleChoiceAnswers,
+
+        @NotNull(message = "Не может быть null, должно быть хотя бы пустым")
+        @Schema(description = "Мапа для ответов на вопросы со множественным выбором. " +
+                "Должна быть хотя бы пустой, но не null. Ключ - ID вопроса, значение - список ID ответов",
+                requiredMode = REQUIRED)
+        Map<UUID, @NotEmpty List<@NotNull UUID>> multipleChoiceAnswers,
+
+        @NotNull(message = "Не может быть null, должно быть хотя бы пустым")
+        @Schema(description = "Мапа для ответов на вопросы с текстовым вводом. " +
+                "Должна быть хотя бы пустой, но не null. Ключ - ID вопроса, значение - текст ответа",
+                requiredMode = REQUIRED)
+        Map<UUID, @NotEmpty String> textAnswers,
+
+        @NotNull(message = "Не может быть null, должно быть хотя бы пустым")
+        @Schema(description = "Мапа для ответов на вопросы с сопоставлением. " +
+                "Должна быть хотя бы пустой, но не null. Ключ - ID вопроса, значение - список из мап, " +
+                "где ключ - ID термина, " +
+                "значение - ID соответствующего определения",
+                requiredMode = REQUIRED)
+        Map<UUID, @NotEmpty List<Map<UUID, @NotNull UUID>>> matchingAnswers
+) {
+}
