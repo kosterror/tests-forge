f4b586ed5d81525168230675dffc6f61a860f885
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-08-18 23:36:56 +0700

    Поправил опечатку в выражении для @PreAuthorize

--- security-starter/src/main/java/ru/kosterror/testsforge/securitystarter/util/RoleExpressions.java
+++ security-starter/src/main/java/ru/kosterror/testsforge/securitystarter/util/RoleExpressions.java
@@ -8,6 +8,6 @@ public class RoleExpressions {
     public static final String TEACHER_OR_STUDENT = "hasAnyRole('ROLE_STUDENT', 'ROLE_TEACHER')";
     public static final String TEACHER = "hasRole('ROLE_TEACHER')";
     public static final String STUDENT = "hasRole('ROLE_STUDENT')";
-    public static final String SERVICE = "hasService('ROLE_SERVICE')";
+    public static final String SERVICE = "hasRole('ROLE_SERVICE')";
     public static final String TEACHER_OR_STUDENT_OR_SERVICE = "hasAnyRole('ROLE_STUDENT', 'ROLE_TEACHER', 'ROLE_SERVICE')";
 }
